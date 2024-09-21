860c630f64cdc7cd3ddcb2545e5b4632b87b8b54
Shinya Takematsu <bpinelab@gmail.com>
2024-09-21 15:44:21 +0900

    bug fix

--- bookstore-app/frontend/pages/index.tsx
+++ bookstore-app/frontend/pages/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import { supabase } from "../lib/supabaseClient";
 import Link from "next/link";
+import { supabase } from "../lib/supabaseClient";
 
 interface Author {
   name: string;
